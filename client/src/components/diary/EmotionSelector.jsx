78d30243bb15e77ea64ca3d3b082a32452c9830a
sohee <01okok@naver.com>
2025-03-28 13:04:52 +0900

    모바일 이미지 변경

--- client/src/components/diary/EmotionSelector.jsx
+++ client/src/components/diary/EmotionSelector.jsx
@@ -4,20 +4,20 @@ import styles from "../../scss/components/DiaryDetail.module.scss";
 export default function EmotionSelector({ category, formData, changeValue }) {
     const emotions = {
         before: [
-            { id: "happy", label: "즐거움", icon: "/icon/happy.svg" },
-            { id: "excited", label: "설렘", icon: "/icon/excited.svg" },
-            { id: "neutral", label: "평온", icon: "/icon/neutral.svg" },
-            { id: "angry", label: "분노", icon: "/icon/angry.svg" },
-            { id: "sad", label: "슬픔", icon: "/icon/sad.svg" },
-            { id: "unpleasant", label: "불쾌", icon: "/icon/unpleasant.svg" },
+            { id: "happy", label: "즐거움", icon: "/icon/happy.png" },
+            { id: "excited", label: "설렘", icon: "/icon/excited.png" },
+            { id: "neutral", label: "평온", icon: "/icon/neutral.png" },
+            { id: "angry", label: "분노", icon: "/icon/angry.png" },
+            { id: "sad", label: "슬픔", icon: "/icon/sad.png" },
+            { id: "unpleasant", label: "불쾌", icon: "/icon/unpleasant.png" },
         ],
         after: [
-            { id: "happy2", label: "즐거움", icon: "/icon/happy2.svg" },
-            { id: "touched2", label: "감동", icon: "/icon/touched2.svg" },
-            { id: "neutral2", label: "평온", icon: "/icon/neutral2.svg" },
-            { id: "angry2", label: "분노", icon: "/icon/angry2.svg" },
-            { id: "sad2", label: "슬픔", icon: "/icon/sad2.svg" },
-            { id: "scared2", label: "공포", icon: "/icon/scared2.svg" },
+            { id: "happy2", label: "즐거움", icon: "/icon/happy2.png" },
+            { id: "touched2", label: "감동", icon: "/icon/touched2.png" },
+            { id: "neutral2", label: "평온", icon: "/icon/neutral2.png" },
+            { id: "angry2", label: "분노", icon: "/icon/angry2.png" },
+            { id: "sad2", label: "슬픔", icon: "/icon/sad2.png" },
+            { id: "scared2", label: "공포", icon: "/icon/scared2.png" },
         ],
     };
     const emotionList =
